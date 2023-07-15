b5d2b4cf1241802cb5b7d67d89739b46241ef75d
karine josien <karine.josien@domo.com>
2023-07-15 11:55:23 -0600

    start main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,38 @@
-function MyButton() {
-  return (
-    <button>
-      hi
-    </button>
-  );
-}
+import React from "react";
+import Search from "./search";
 
-export default function MyApp() {
+
+export default class MyApp extends React.Component<any, any>{
+
+updateSearchValue = (value: string, type:string) => {
+  this.setState({ searchValue: value, searchType: type });
+  if (type === "team") {
+    this.setState({ teamNumber: value 
+    , teamName:null});
+  }
+  if (type === "name"){
+    this.setState({ teamName: value, teamNumber: null });
+     
+  }
+  
+}
+render() {
+
   return (
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+      <h1>Title</h1>
+      <Search
+        updateSearchValue={this.updateSearchValue}
+        searchValue={this.state.searchValue}
+        //searchType={this.state.searchType}
+      >
+      </Search>
+      <Map></Map>
+      <Detail
+        teamName={this.state.teamName}
+        teamNunmber={this.state.teamNumber}
+      ></Detail>
     </div>
   );
+}
 }
